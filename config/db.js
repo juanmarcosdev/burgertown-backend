475ee5996a961d84b7cff6386a23e116a0b233d2
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-10-20 16:05:00 -0500

    Tratando configurar PG Heroku

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
 const Sequelize = require('sequelize');
 module.exports  = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
+    protocol: 'postgres',
+    logging:  true //false
 })
