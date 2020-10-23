ce607ba13784023fbe21a13c05722751550c569d
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-10-23 14:51:12 -0500

    Trying to fix CORS 4

--- app.js
+++ app.js
@@ -10,17 +10,10 @@ const rutasSede = require('./routes/Sede')
 //Rutas importe
 
 //Middlewares
-// app.use(cors());
+app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 
 
 //Rutas cliente
