832fc71d99466aa88f716287392c5a0d38489ad5
Pascu999 <cristian.pascumal@correounivalle.edu.co>
2020-10-24 15:13:30 -0500

    Añadiendo jwt a clientes y usuarios

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -1,153 +1,198 @@
-const Cliente = require('../models/Cliente');
-const Encriptacion = require('../middleware/Encriptacion')
+const Cliente = require("../models/Cliente");
+const Encriptacion = require("../middleware/Encriptacion");
+const jwt = require("jsonwebtoken");
+const { jwtGeneratorCliente } = require("../utils/jwtGenerator");
+
 
 async function getCliente(req, res) {
-    const { cliente_id } = req.params
-    try {
-        let getCliente = await Cliente.findOne({
+  const { cliente_id } = req.params;
+  try {
+    let getCliente = await Cliente.findOne({
+      where: {
+        cliente_id,
+      },
+    });
 
-            where: {
-                cliente_id
-            }
-        })
-
-        res.json({
-            data: getCliente
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            message: "Error",
-            data: {}
-        })
-    }
-
+    res.json({
+      data: getCliente,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error",
+      data: {},
+    });
+  }
 }
 
 async function getClientes(req, res) {
-
-    try {
-        const Clientes = await Cliente.findAll();
-        res.json({
-            data: Clientes
-        })
-
-    } catch (error) {
-
-    }
-
+  try {
+    const Clientes = await Cliente.findAll();
+    res.json({
+      data: Clientes,
+    });
+  } catch (error) {}
 }
 
 async function createCliente(req, res) {
-    //console.log(req.body);
-    const { cliente_celular, cliente_nombre, cliente_apellido, cliente_documento, cliente_direccion, cliente_fecha_nacimiento, cliente_password } = req.body
-    const bcryptPassword = await Encriptacion.Encriptar(cliente_password)
-    try {
-        let newCliente = await Cliente.create({
-            cliente_celular,
-            cliente_nombre,
-            cliente_apellido,
-            cliente_documento,
-            cliente_direccion,
-            cliente_fecha_nacimiento,
-            cliente_password:bcryptPassword
-        }, {
-            fields: ['cliente_celular', 'cliente_nombre', 'cliente_apellido', 'cliente_documento', 'cliente_direccion', 'cliente_fecha_nacimiento', 'cliente_password']
-        });
+  const {
+    cliente_celular,
+    cliente_nombre,
+    cliente_apellido,
+    cliente_documento,
+    cliente_direccion,
+    cliente_fecha_nacimiento,
+    cliente_password,
+  } = req.body;
+  const bcryptPassword = await Encriptacion.Encriptar(cliente_password);
+  try {
+    let newCliente = await Cliente.create(
+      {
+        cliente_celular,
+        cliente_nombre,
+        cliente_apellido,
+        cliente_documento,
+        cliente_direccion,
+        cliente_fecha_nacimiento,
+        cliente_password: bcryptPassword,
+      },
+      {
+        fields: [
+          "cliente_celular",
+          "cliente_nombre",
+          "cliente_apellido",
+          "cliente_documento",
+          "cliente_direccion",
+          "cliente_fecha_nacimiento",
+          "cliente_password",
+        ],
+      }
+    );
 
-        if (newCliente) {
-            res.json({
-                meesage: 'Cliente agregado satisfactoriamente',
-                data: newCliente
-            })
-        }
+    if (newCliente) {
+      const token = jwtGeneratorCliente(cliente_celular);
 
-
-    } catch (error) {
-        res.status(500).json({
-            meesage: 'Error al ingresar el cliente',
-            data: error
-        })
-            ;
-
+      res.json({
+        meesage: "Cliente agregado satisfactoriamente",
+        data: newCliente,
+        token,
+      });
     }
-
-
-
+  } catch (error) {
+    res.status(500).json({
+      meesage: "Error al ingresar el cliente",
+      data: error,
+    });
+  }
 }
 
 async function stateCliente(req, res) {
-    const { cliente_id } = req.params;
-    const clientes = await Cliente.findAll({
-        attributes: ['cliente_id', 'cliente_estado'],
-        where: {
-            cliente_id
-        }
-    });
-    try {
-        if (clientes.length > 0) {
-            clientes.forEach(async cliente => {
-                console.log(cliente.cliente_estado);
-                nuevoestado = cliente.cliente_estado == 0 ? 1 : 0;
-                await cliente.update({
-                    cliente_estado : nuevoestado
-                });
+  const { cliente_id } = req.params;
+  const clientes = await Cliente.findAll({
+    attributes: ["cliente_id", "cliente_estado"],
+    where: {
+      cliente_id,
+    },
+  });
+  try {
+    if (clientes.length > 0) {
+      clientes.forEach(async (cliente) => {
+        console.log(cliente.cliente_estado);
+        nuevoestado = cliente.cliente_estado == 0 ? 1 : 0;
+        await cliente.update({
+          cliente_estado: nuevoestado,
+        });
+      });
 
-            })
-
-            return res.json({
-                meesage: 'Cliente deshabilitado/habilitadoesatisfactoriamente',
-                data: clientes
-            })
-
-        }
-
-    } catch (error) {
-        console.log(error);
+      return res.json({
+        meesage: "Cliente deshabilitado/habilitadoesatisfactoriamente",
+        data: clientes,
+      });
     }
-
-
-
-
-
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function editCliente(req, res) {
-    const { cliente_id } = req.params;
-    const { cliente_nombre, cliente_apellido, cliente_documento, cliente_direccion, cliente_fecha_nacimiento, cliente_password } = req.body;
+  const { cliente_id } = req.params;
+  const {
+    cliente_nombre,
+    cliente_apellido,
+    cliente_documento,
+    cliente_direccion,
+    cliente_fecha_nacimiento,
+    cliente_password,
+  } = req.body;
 
-    const clientes = await Cliente.findAll({
-        attributes: ['cliente_id', 'cliente_celular', 'cliente_nombre', 'cliente_apellido', 'cliente_documento', 'cliente_direccion', 'cliente_fecha_nacimiento', 'cliente_password'],
+  const clientes = await Cliente.findAll({
+    attributes: [
+      "cliente_id",
+      "cliente_celular",
+      "cliente_nombre",
+      "cliente_apellido",
+      "cliente_documento",
+      "cliente_direccion",
+      "cliente_fecha_nacimiento",
+      "cliente_password",
+    ],
 
-        where: {
-            cliente_id
-        },
+    where: {
+      cliente_id,
+    },
+  });
 
-    })
-
-    if (clientes.length > 0) {
-        clientes.forEach(async cliente => {
-            await cliente.update({
-                cliente_nombre,
-                cliente_apellido,
-                cliente_documento,
-                cliente_direccion,
-                cliente_fecha_nacimiento,
-                cliente_password
-            });
-
-        })
-    }
-    return res.json({
-        message: 'Datos cambiados correctamente',
-        data: clientes
-    })
-
+  if (clientes.length > 0) {
+    clientes.forEach(async (cliente) => {
+      await cliente.update({
+        cliente_nombre,
+        cliente_apellido,
+        cliente_documento,
+        cliente_direccion,
+        cliente_fecha_nacimiento,
+        cliente_password,
+      });
+    });
+  }
+  return res.json({
+    message: "Datos cambiados correctamente",
+    data: clientes,
+  });
 }
 
+async function loginCliente(req, res) {
+  const { cliente_celular } = req.params;
+  const { cliente_password } = req.body;
+
+  let getCliente = await Cliente.findOne({
+    where: {
+      cliente_celular
+    },
+  });
+
+  if (getCliente) {
+    try {
+      const compararContraseña = await Encriptacion.Comparar(
+        cliente_password,
+        getCliente.cliente_password
+      );
+
+      const token = await jwtGeneratorCliente(cliente_celular);
+
+      return res.json({ 
+          message: "login exitoso" ,
+          token
+        });
+    } catch (error) {
+      res.json({
+        message: "Error",
+      });
+    }
+  }
+}
 
 exports.createCliente = createCliente;
 exports.getClientes = getClientes;
 exports.getCliente = getCliente;
 exports.stateCliente = stateCliente;
 exports.editCliente = editCliente;
+exports.loginCliente = loginCliente;
--- controllers/trabajador.controller.js
+++ controllers/trabajador.controller.js
@@ -1,155 +1,208 @@
-const Trabajador = require('../models/Trabajador');
-const Encriptacion = require('../middleware/Encriptacion')
+const Trabajador = require("../models/Trabajador");
+const Encriptacion = require("../middleware/Encriptacion");
+const jwt = require("jsonwebtoken");
+const { jwtGeneratorTrabajador } = require("../utils/jwtGenerator");
+
 
 async function getTrabajador(req, res) {
-    const { trabajador_id } = req.params;
-    try {
-        let getTrabajador = await Trabajador.findOne({
+  const { trabajador_id } = req.params;
+  try {
+    let getTrabajador = await Trabajador.findOne({
+      where: {
+        trabajador_id,
+      },
+    });
 
-            where: {
-                trabajador_id
-            }
-        })
-
-        res.json({
-            data: getTrabajador
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            message: "Error",
-            data: {}
-        })
-    }
-
+    res.json({
+      data: getTrabajador,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error",
+      data: {},
+    });
+  }
 }
 
 async function getTrabajadores(req, res) {
-
-    try {
-        const Trabajadores = await Trabajador.findAll();
-        res.json({
-            data: Trabajadores
-        })
-
-    } catch (error) {
-
-    }
-
+  try {
+    const Trabajadores = await Trabajador.findAll();
+    res.json({
+      data: Trabajadores,
+    });
+  } catch (error) {}
 }
 
 async function createTrabajador(req, res) {
-    const { trabajador_documento, trabajador_nombre, trabajador_apellido, trabajador_celular, trabajador_foto, trabajador_contratacion, trabajador_cargo, trabajador_direccion, trabajador_password } = req.body
-    const bcryptPassword = await Encriptacion.Encriptar(trabajador_password);
-    try {
-        let newTrabajador = await Trabajador.create({
-            trabajador_documento,
-            trabajador_nombre,
-            trabajador_apellido,
-            trabajador_celular,
-            trabajador_foto,
-            trabajador_contratacion,
-            trabajador_cargo,
-            trabajador_direccion,
-            trabajador_password: bcryptPassword
-        }, {
-            fields: ['trabajador_documento', 'trabajador_nombre', 'trabajador_apellido', 'trabajador_celular', 'trabajador_foto', 'trabajador_contratacion', 'trabajador_cargo', 'trabajador_direccion', 'trabajador_password']
-        });
+  const {
+    trabajador_documento,
+    trabajador_nombre,
+    trabajador_apellido,
+    trabajador_celular,
+    trabajador_foto,
+    trabajador_contratacion,
+    trabajador_cargo,
+    trabajador_direccion,
+    trabajador_password,
+  } = req.body;
+  const bcryptPassword = await Encriptacion.Encriptar(trabajador_password);
+  try {
+    let newTrabajador = await Trabajador.create(
+      {
+        trabajador_documento,
+        trabajador_nombre,
+        trabajador_apellido,
+        trabajador_celular,
+        trabajador_foto,
+        trabajador_contratacion,
+        trabajador_cargo,
+        trabajador_direccion,
+        trabajador_password: bcryptPassword,
+      },
+      {
+        fields: [
+          "trabajador_documento",
+          "trabajador_nombre",
+          "trabajador_apellido",
+          "trabajador_celular",
+          "trabajador_foto",
+          "trabajador_contratacion",
+          "trabajador_cargo",
+          "trabajador_direccion",
+          "trabajador_password",
+        ],
+      }
+    );
 
-        if (newTrabajador) {
-            res.json({
-                meesage: 'Trabajador agregado satisfactoriamente',
-                data: newTrabajador
-            })
-        }
+    if (newTrabajador) {
+      const token = jwtGeneratorTrabajador(trabajador_documento);
 
-
-    } catch (error) {
-        res.status(500).json({
-            meesage: 'Error al ingresar el Trabajador',
-            data: {}
-        })
-            ;
-
+      res.json({
+        meesage: "Trabajador agregado satisfactoriamente",
+        data: newTrabajador,
+        token,
+      });
     }
-
-
-
+  } catch (error) {
+    res.status(500).json({
+      meesage: "Error al ingresar el Trabajador",
+      data: {},
+    });
+  }
 }
 
 async function stateTrabajador(req, res) {
-    const { trabajador_id } = req.params;
-    const trabajadores = await Trabajador.findAll({
-        attributes: ['trabajador_id', 'trabajador_estado'],
-        where: {
-            trabajador_id
-        }
-    });
-    try {
-        if (trabajadores.length > 0) {
-            trabajadores.forEach(async trabajador => {
-                nuevoestado = trabajador.trabajador_estado == 0 ? 1 : 0;
-                await trabajador.update({
-                    trabajador_estado: nuevoestado
-                });
+  const { trabajador_id } = req.params;
+  const trabajadores = await Trabajador.findAll({
+    attributes: ["trabajador_id", "trabajador_estado"],
+    where: {
+      trabajador_id,
+    },
+  });
+  try {
+    if (trabajadores.length > 0) {
+      trabajadores.forEach(async (trabajador) => {
+        nuevoestado = trabajador.trabajador_estado == 0 ? 1 : 0;
+        await trabajador.update({
+          trabajador_estado: nuevoestado,
+        });
+      });
 
-            })
-
-            return res.json({
-                meesage: 'Trabajador deshabilitado/habilitado satisfactoriamente',
-                data: trabajadores
-            })
-
-        }
-
-    } catch (error) {
-        console.log(error);
+      return res.json({
+        meesage: "Trabajador deshabilitado/habilitado satisfactoriamente",
+        data: trabajadores,
+      });
     }
-
-
-
-
-
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function editTrabajador(req, res) {
-    const { trabajador_id } = req.params;
-    const { trabajador_nombre, trabajador_apellido, trabajador_celular, trabajador_foto, trabajador_contratacion, trabajador_cargo, trabajador_direccion, trabajador_password } = req.body;
+  const { trabajador_id } = req.params;
+  const {
+    trabajador_nombre,
+    trabajador_apellido,
+    trabajador_celular,
+    trabajador_foto,
+    trabajador_contratacion,
+    trabajador_cargo,
+    trabajador_direccion,
+    trabajador_password,
+  } = req.body;
 
-    const Trabajadores = await Trabajador.findAll({
-        attributes: ['trabajador_id', 'trabajador_nombre', 'trabajador_apellido', 'trabajador_celular', 'trabajador_foto', 'trabajador_contratacion', 'trabajador_cargo', 'trabajador_direccion', 'trabajador_password'],
+  const Trabajadores = await Trabajador.findAll({
+    attributes: [
+      "trabajador_id",
+      "trabajador_nombre",
+      "trabajador_apellido",
+      "trabajador_celular",
+      "trabajador_foto",
+      "trabajador_contratacion",
+      "trabajador_cargo",
+      "trabajador_direccion",
+      "trabajador_password",
+    ],
 
-        where: {
-            trabajador_id
-        },
+    where: {
+      trabajador_id,
+    },
+  });
 
-    })
-
-    if (Trabajadores.length > 0) {
-        Trabajadores.forEach(async Trabajador => {
-            await Trabajador.update({
-                trabajador_nombre,
-                trabajador_apellido,
-                trabajador_celular,
-                trabajador_foto,
-                trabajador_contratacion,
-                trabajador_cargo,
-                trabajador_direccion,
-                trabajador_password
-            });
-
-        })
-    }
-    return res.json({
-        message: 'Datos del trabajador cambiados correctamente',
-        data: Trabajadores
-    })
-
+  if (Trabajadores.length > 0) {
+    Trabajadores.forEach(async (Trabajador) => {
+      await Trabajador.update({
+        trabajador_nombre,
+        trabajador_apellido,
+        trabajador_celular,
+        trabajador_foto,
+        trabajador_contratacion,
+        trabajador_cargo,
+        trabajador_direccion,
+        trabajador_password,
+      });
+    });
+  }
+  return res.json({
+    message: "Datos del trabajador cambiados correctamente",
+    data: Trabajadores,
+  });
 }
 
+async function loginTrabajador(req, res) {
+  const { trabajador_documento } = req.params;
+  const { trabajador_password } = req.body;
+
+  let getTrabajador = await Trabajador.findOne({
+    where: {
+      trabajador_documento,
+    },
+  });
+
+  if (getTrabajador) {
+    try {
+      const compararContraseña = await Encriptacion.Comparar(
+        trabajador_password,
+        getTrabajador.cliente_password
+      );
+
+      const token = await jwtGeneratorTrabajador(trabajador_documento);
+
+      return res.json({
+        message: "login exitoso",
+        token,
+      });
+    } catch (error) {
+      res.json({
+        message: "Error",
+      });
+    }
+  }
+}
 
 exports.createTrabajador = createTrabajador;
 exports.getTrabajadores = getTrabajadores;
 exports.getTrabajador = getTrabajador;
 exports.stateTrabajador = stateTrabajador;
 exports.editTrabajador = editTrabajador;
+exports.loginTrabajador = loginTrabajador;
