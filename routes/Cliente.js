d1a53a225b05366db7a53b767bfea047c3140557
Pascu999 <cristian.pascumal@correounivalle.edu.co>
2020-11-15 22:47:34 -0500

    Revirtiendo error en ruta editar

--- routes/Cliente.js
+++ routes/Cliente.js
@@ -13,7 +13,7 @@ router.post('/Tarjeta/Add',createTarjeta);
 //    /api/:cliente_celular
 router.get('/:cliente_id',autorizacionTrabajador,getCliente);
 router.put('/:cliente_id',autorizacionTrabajador,stateCliente);
-router.put('/Edit/:cliente_id',autorizacionCliente,editCliente);
+router.put('/Edit/:cliente_id',autorizacionTrabajador,editCliente);
 router.put('/Login/:cliente_celular',loginCliente);
 
 module.exports = router;
