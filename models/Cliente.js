f00711f557d1cf13cbd65d10677bb36cef273eb4
Pascu999 <cristian.pascumal@correounivalle.edu.co>
2020-12-01 15:00:40 -0500

    Solucionando problema con el backend

---
+++ models/Cliente.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize')
+const db = require('../config/db')
+
+const Cliente = db.define("clientes",{
+    cliente_id  :{
+        type: Sequelize.INTEGER,
+        primaryKey:true,
+
+    },
+
+    cliente_celular:{
+        type: Sequelize.CHAR,
+        
+
+    },
+    cliente_nombre:{
+        type: Sequelize.CHAR,
+        allowNull: false
+
+    },
+    cliente_apellido:{
+        type: Sequelize.CHAR,
+        allowNull: false
+    },
+    cliente_documento:{
+        type: Sequelize.CHAR,
+        allowNull: false
+
+    },
+    cliente_direccion:{
+        type: Sequelize.CHAR,
+        allowNull: false
+
+    },
+    cliente_fecha_nacimiento:{
+        type: Sequelize.DATEONLY,
+        allowNull: false
+
+    },
+    cliente_password:{
+        type: Sequelize.CHAR,
+        allowNull: false
+
+    },
+    cliente_estado : {
+        type: Sequelize.INTEGER,
+        default: 0
+    },
+    cliente_foto : {
+        type: Sequelize.CHAR,
+    }
+},{
+    timestamps : false
+});
+
+module.exports = Cliente;
