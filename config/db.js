333808e677cd68ba4549d80ad0b98be37ffb5519
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2021-05-01 23:17:48 -0500

    Tratando de arreglar error habilitando SSL en Pgconnection con Heroku 2

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ const Sequelize = require('sequelize');
 if(process.env.DATABASE_URL) {
     module.exports  = new Sequelize(process.env.DATABASE_URL, {
         dialect:  'postgres',
-        "dialectOptions": {
-            "ssl": true
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            }
           },
         protocol: 'postgres',
         logging:  true
@@ -12,8 +15,11 @@ if(process.env.DATABASE_URL) {
     module.exports  = new Sequelize('ds2','postgres','11111',{
         host:'localhost',
         dialect:'postgres',
-        "dialectOptions": {
-            "ssl": true
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            }
           },
     })
 }
